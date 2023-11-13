f36d3e222fe4aba79abc9706c2d3575f259b5815
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-11-13 10:12:41 +0200

    Add vue example

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="btn btn-primary" @click="message='Hello MM22'">Click me!</button>
+    <input type="text" 
+        class="form-control" 
+        :value="message" 
+        @input="message=$event.target.value">
+    <input type="text" class="form-control" v-model="message">
+    <h1>{{ message }}</h1>
+    <button class="btn btn-danger" @click="counter++">Count is {{ counter }}</button>
+    <button class="btn"
+        :class="{'btn-danger': !btnState, 'btn-success': btnState}"
+        @click="btnState=!btnState">Toggle</button>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message: 'Hello Vue',
+            counter: 0,
+            btnState: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import esm from './esm.js';
-esm();
-//const cjs = require('./common.js');
-//cjs();
 import './style.scss';
 
 import * as bootstrap from 'bootstrap';
+import { createApp } from 'vue';
+
+import App from './App.vue';
+
+const app = createApp(App);
+app.mount('#app');
