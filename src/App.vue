4a24264f7c796f3ad507f4c3a262a37de9bcb522
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-11-15 15:11:51 +0200

    Add ItemList component

--- src/App.vue
+++ src/App.vue
@@ -4,73 +4,47 @@
         <input type="text" class="form-control" v-model="message" @keydown.enter="add()">
         <button class="btn btn-primary" @click="add()">Add</button>
     </div>
-    <h1>All Items</h1>
-    <ul>
-        <li v-for="item in items" :key="item.id">
-            <div class="form-check form-switch">
-                <label>
-                    {{ item.name }}
-                    <input type="checkbox" class="form-check-input" v-model="item.isDone">
-                </label>
-            </div>
-        </li>
-    </ul>
-    <h1>Done Items</h1>
-    <ul>
-        <li v-for="item in doneItems" :key="item.id">
-            <div class="form-check form-switch">
-                <label>
-                    {{ item.name }}
-                    <input type="checkbox" class="form-check-input" v-model="item.isDone">
-                </label>
-            </div>
-        </li>
-    </ul>
-    <h1>ToDo Items</h1>
-    <ul>
-        <li v-for="item in toDoItems" :key="item.id">
-            <div class="form-check form-switch">
-                <label>
-                    {{ item.name }}
-                    <input type="checkbox" class="form-check-input" v-model="item.isDone">
-                </label>
-            </div>
-        </li>
-    </ul>
+    <ItemList :items="items" title="All Items"></ItemList>
+    <ItemList :items="doneItems" title="Done Items"></ItemList>
+    <ItemList :items="toDoItems" title="ToDo Items"></ItemList>
+    
 </div>
 </template>
 
 <script>
+import ItemList from './components/ItemList.vue';
+
 let id = 0;
 
 export default {
-    data(){
+    data() {
         return {
             message: '',
             items: [
-                {id: id++,name:'Piim', isDone: false},
-                {id: id++,name:'Sai', isDone: true},
-                {id: id++,name:'Viin', isDone: false},
-                {id: id++,name:'Leib', isDone: true},
+                { id: id++, name: 'Piim', isDone: false },
+                { id: id++, name: 'Sai', isDone: true },
+                { id: id++, name: 'Viin', isDone: false },
+                { id: id++, name: 'Leib', isDone: true },
             ]
-        }
+        };
     },
     methods: {
-        add(){
-            if(this.message.trim() !== ''){
-                this.items.push({id: id++, name: this.message.trim(), isDone: false});
+        add() {
+            if (this.message.trim() !== '') {
+                this.items.push({ id: id++, name: this.message.trim(), isDone: false });
             }
             this.message = '';
         }
     },
     computed: {
-        doneItems(){
+        doneItems() {
             return this.items.filter(item => item.isDone);
         },
-        toDoItems(){
+        toDoItems() {
             return this.items.filter(item => !item.isDone);
         }
-    }
+    },
+    components: { ItemList }
 }
 </script>
 
