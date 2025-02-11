22048d730df04952b611286338ef2db8c8218a41
Marlon.rns <Marlon.ranisoo@tptlive.ee>
2025-02-11 23:59:22 +0200

    Cookie Clicker with Timer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,59 @@
 <template>
-    <Tabs></Tabs>
-    <router-view></router-view>
-  
+  <div id="app">
+    <h1>Cookie Clicker</h1>
+    <p>Time Left: {{ timeLeft }}s</p>
+    <button @click="clickCookie" :disabled="timeLeft === 0">
+      🍪 Click the Cookie
+    </button>
+    <p>Cookies: {{ cookies }}</p>
+    <button v-if="timeLeft === 0" @click="restartGame">Restart</button>
+  </div>
 </template>
+
 <script>
-import Tabs from './components/Tabs.vue';
 export default {
-    components: {
-        Tabs
+  data() {
+    return {
+      cookies: 0,
+      timeLeft: 60,
+      timer: null,
+    };
+  },
+  methods: {
+    clickCookie() {
+      if (this.timeLeft > 0) {
+        this.cookies++;
+      }
+    },
+    startTimer() {
+      this.timer = setInterval(() => {
+        if (this.timeLeft > 0) {
+          this.timeLeft--;
+        } else {
+          clearInterval(this.timer);
+        }
+      }, 1000);
+    },
+    restartGame() {
+      this.cookies = 0;
+      this.timeLeft = 60;
+      this.startTimer();
     }
-}
+  },
+  mounted() {
+    this.startTimer();
+  }
+};
 </script>
+
+<style>
+#app {
+  text-align: center;
+  font-family: Arial, sans-serif;
+}
+button {
+  font-size: 18px;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
